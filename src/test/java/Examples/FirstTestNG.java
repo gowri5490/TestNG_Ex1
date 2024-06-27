d15696712e5b21871306929536565cb3e65ab289
Gowrisankar <gowrikavin54@gmail.com>
2024-06-27 12:00:50 +0530

    commit

---
+++ src/test/java/Examples/FirstTestNG.java
@@ -0,0 +1,24 @@
+package Examples;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNG {
+  
+	@Test
+	void test1()
+	{
+		System.out.println("Testcase 1");
+	}
+	
+	@Test
+	void test2()
+	{
+		System.out.println("Testcase 2");
+	}
+	
+	@Test
+	void test3()
+	{
+		System.out.println("Testcase 3");
+	}
+}
